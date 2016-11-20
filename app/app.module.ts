47b60b81b7a55916391227a03a3092edd8239711
markus-schneider12 <markus.schneider12@allianz.de>
2016-11-21 00:45:09 +0100

    finished tutorial step 6 Routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,24 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
 
+// components
 import {AppComponent}   from './app.component';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent} from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
+
+// services
+import {HeroService} from './hero.service';
+
+// routing
+import {AppRoutingModule} from './app-routing.module';
 
 
 @NgModule({
-    imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent, HeroDetailComponent],
+    imports: [BrowserModule, FormsModule, AppRoutingModule],
+    declarations: [AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
+    providers: [HeroService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -8,7 +8,17 @@ export class HeroService {
     getHeroes(): Promise<Hero[]> {
         //return HEROES;
         return Promise.resolve(HEROES);
-
     }
+
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+    }
+
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(resolve, 2000)) // delay 2 seconds
+            .then(() => this.getHeroes());
+    }
+
 
 }
